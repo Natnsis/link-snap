84754ad0a0172e6137ea6055498b171b57520f62
Nati <nsisay49@gmail.com>
2026-01-13 18:39:22 +0300

    logo

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -1,12 +1,10 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio"
 const LandingHero = () => {
   return (
     <div className="flex items-center justify-center">
       <div>
-        <AspectRatio ratio={16 / 9}>
-          <Image src="..." alt="Image" className="rounded-md object-cover" />
-        </AspectRatio>
-        <img src="/inner/link.png" />
+        <div>
+          <img src="/inner/link.png" alt="Image" className="rounded-md object-cover h-50" />
+        </div>
         <h1 className="text-3xl font-extrabold">LinkSnap</h1>
         <p>Your content, organized and beautifully previewed.</p>
       </div>
