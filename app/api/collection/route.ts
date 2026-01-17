99fba8b515247ba73882ae9934ad362d8f23a038
Nati <nsisay49@gmail.com>
2026-01-17 19:39:02 +0300

    collection controllers

---
+++ app/api/collection/route.ts
@@ -0,0 +1,66 @@
+import { createClient } from "@/utils/supabase/client"
+
+export const getCollections = async () => {
+  try {
+    const supabase = createClient()
+    const { data: collections, error: collectionError } = await supabase.from('collection')
+      .select('*')
+    if (collectionError) throw collectionError
+    return collections
+  } catch (error) {
+    throw error
+  }
+}
+
+export const deleteCollecion = async (id: string) => {
+  try {
+    const supabase = createClient()
+    const { error: deletionError } = await supabase.from('collection')
+      .delete()
+      .eq('id', id)
+    if (deletionError) throw deletionError
+  } catch (error) {
+    throw error
+  }
+}
+
+export const createCollection = async (collectionData, id) => {
+  try {
+    const supabase = createClient()
+    const { title, description, tag1, tag2, tag3, user_id } = collectionData
+    const { error: collectionError } = await supabase.from('collection')
+      .insert([
+        {
+          title,
+          description,
+          tag1,
+          tag2,
+          tag3,
+          user_id: id
+        }
+      ])
+    if (collectionError) throw collectionError
+  } catch (error) {
+    throw error
+  }
+}
+
+export const updateCollection = async (collectionData, id) => {
+  try {
+    const supabase = createClient()
+    const { title, description, tag1, tag2, tag3 } = collectionData;
+    const { error: updateError } = await supabase.from('collection')
+      .update({
+        title,
+        description,
+        tag1,
+        tag2,
+        tag3,
+      })
+      .eq('id', id)
+      .select()
+    if (updateError) throw updateError
+  } catch (error) {
+    throw error
+  }
+}
