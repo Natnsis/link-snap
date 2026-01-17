2076bcfcfbb76692244606c5ac321f2b8d51096b
Nati <nsisay49@gmail.com>
2026-01-17 19:10:24 +0300

    link rest

--- app/api/link/route.ts
+++ app/api/link/route.ts
@@ -21,4 +21,44 @@ export const addLink = async (id, data) => {
   }
 }
 
+export const getAllLinks = async () => {
+  try {
+    const supabase = createClient()
+    const { data: linkData, error: linkError } = await supabase.from('link')
+      .select('*')
+    if (linkError) throw linkError
+    return linkData;
+  } catch (error) {
+    throw error;
 
+  }
+}
+
+export const deleteLinks = async (linkId) => {
+  try {
+    const supabase = createClient()
+    const { error: linkError } = await supabase.from('link')
+      .delete()
+      .eq('id', 'linkId')
+    if (linkError) throw linkError
+  } catch (error) {
+    throw error
+  }
+}
+
+export const updateLink = async (updateData, linkId) => {
+  try {
+    const supabase = createClient()
+    const { data: linkData, error: updateError } = await supabase.from('link')
+      .update({
+        title: updateData.title,
+        priority: updateData.priority,
+        purpose: updateData.purpose,
+        url: updateData.url
+      })
+    if (updateError) throw updateError
+    return linkData;
+  } catch (error) {
+    throw error
+  }
+}
