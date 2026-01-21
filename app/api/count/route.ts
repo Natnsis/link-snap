8e65a581b5bcd5970dd99515d1b455ab72a8c38d
Nati <nsisay49@gmail.com>
2026-01-21 19:50:08 +0300

    counts insertion

--- app/api/count/route.ts
+++ app/api/count/route.ts
@@ -1,33 +1,38 @@
 import { createClient } from "@/utils/supabase/client"
 
-export const linkCount = async () => {
-  try {
-    const supabase = createClient();
-    const { count: link, error } = await supabase.from("links")
-      .select("*", { count: "exact", head: true })
-    if (error) throw error
-    return link;
-  } catch (e) {
-    throw e
-  }
-};
+export const linkCount = async (): Promise<number> => {
+  const supabase = createClient()
 
-export const collectionCount = async () => {
-  try {
-    const supabase = createClient();
-    const { count: collection, error } = await supabase.from("collection")
-      .select("*", { count: "exact", head: true })
-    if (error) throw error
-    return collection
-  } catch (e) {
-    throw e
-  }
+  const { count, error } = await supabase
+    .from("link")
+    .select("*", { count: "exact", head: true })
+
+  if (error) throw error
+
+  return count ?? 0
+}
+
+export const collectionCount = async (): Promise<number> => {
+  const supabase = createClient()
+
+  const { count, error } = await supabase
+    .from("collection")
+    .select("*", { count: "exact", head: true })
+
+  if (error) throw error
+
+  return count ?? 0
 }
 
 export const favoriteCount = async () => {
   try {
     const supabase = createClient();
     const { count: favorite, error } = await supabase.from("favorite")
+      .select("*", { count: "exact", head: true })
+    if (error) throw error
+    return favorite
+  } catch (e) {
+    throw e
   }
 }
 
--- components/Counts.tsx
+++ components/Counts.tsx
@@ -1,18 +1,30 @@
 "use client"
+import { collectionCount, linkCount } from "@/app/api/count/route";
+import { useQuery } from "@tanstack/react-query";
 import CountUp from "react-countup";
 
 const Counts = () => {
+  const { data: link, isLoading: linking } = useQuery({
+    queryKey: ["linkCount"],
+    queryFn: linkCount
+  })
+
+  const { data: collection, isLoading: collecting } = useQuery({
+    queryKey: ["collectionCount"],
+    queryFn: collectionCount
+  })
+
   return (
     <main className="flex justify-center mt-20">
       <div className="sm:w-6/9 sm:flex sm:justify-between grid grid-rows-3 gap-5">
         <div>
-          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={1289} duration={2} /></h1>
+          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={link ? link : 1204} duration={2} /></h1>
           <p className="text-center text-gray-500">Total Overall links</p>
         </div>
 
         <div>
-          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={34} duration={2} /></h1>
-          <p className="text-center text-gray-500">Saved Today</p>
+          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={collection ? collection : 34} duration={2} /></h1>
+          <p className="text-center text-gray-500">Overall Collections</p>
         </div>
 
         <div>
