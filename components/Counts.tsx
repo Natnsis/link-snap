e61daadbcf5fbc686168ceb2cb8aa52fe0cafa36
Nati <nsisay49@gmail.com>
2026-01-24 12:05:33 +0300

    minor changes'

--- components/Counts.tsx
+++ components/Counts.tsx
@@ -9,27 +9,22 @@ const Counts = () => {
     queryFn: linkCount
   })
 
-  const { data: collection, isLoading: collecting } = useQuery({
-    queryKey: ["collectionCount"],
-    queryFn: collectionCount
-  })
-
   return (
     <main className="flex justify-center mt-20">
       <div className="sm:w-6/9 sm:flex sm:justify-between grid grid-rows-3 gap-5">
         <div>
-          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={link ? link : 1204} duration={2} /></h1>
-          <p className="text-center text-gray-500">Total Overall links</p>
-        </div>
-
-        <div>
-          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={collection ? collection : 34} duration={2} /></h1>
-          <p className="text-center text-gray-500">Overall Collections</p>
+          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={34} duration={2} /></h1>
+          <p className="text-center text-gray-500">UnWatched</p>
         </div>
 
         <div>
           <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={476} duration={2} /></h1>
-          <p className="text-center text-gray-500">Favorites</p>
+          <p className="text-center text-gray-500">Watched</p>
+        </div>
+
+        <div>
+          <h1 className="text-6xl font-extrabold text-center"><CountUp start={0} end={46} duration={2} /></h1>
+          <p className="text-center text-gray-500">Watching</p>
         </div>
       </div>
     </main>
